b961f49d45bd659785cafc73ae03fd1c778a87bf
Kopano-Damane-BYUI <kdamane@byupathway.edu>
2025-03-09 13:04:32 +0200

    Term 2 home page

---
+++ scrips/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year and display it in the footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get the last modified date of the document and display it
+document.getElementById("lastModified").textContent = document.lastModified;
